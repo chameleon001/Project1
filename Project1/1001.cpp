2c96da2e8618b8bb620d1dfa15638b59f17d6b52
chameleon001 <chameleon001@naver.com>
2017-07-31 22:27:53 +0900

    d

---
+++ Project1/1001.cpp
@@ -0,0 +1,22 @@
+/*
+
+
+
+A-B를 계산하시오.
+입력
+
+첫째 줄에 A와 B가 주어진다. (0< A,B < 10)
+출력
+
+첫째 줄에 A-B를 출력한다.
+
+
+*/
+
+#include <stdio.h>
+int main() {
+	int a, b;
+	scanf_s("%d %d", &a, &b);
+	printf("%d\n", a - b);
+	return 0;
+}
