7857ff5703460b8009503fe6f4a86b12d36a5157
chameleon001 <chameleon001@naver.com>
2017-08-28 21:44:37 +0900

    20170828

--- Project1/4344.cpp
+++ Project1/4344.cpp
@@ -27,3 +27,65 @@
 66.667%
 55.556%
 */
+
+#include<stdio.h>
+
+int main() {
+	int t_c, N, i, j, k, score[1001] = { 0, };
+
+
+
+	scanf("%d", &t_c);
+
+
+
+	for (i = 0; i < t_c; i++) {
+
+
+
+		int sum = 0, cnt = 0;
+
+		double avg = 0.0;
+
+
+
+		scanf("%d", &N);
+
+
+		for (j = 0; j < N; j++) {
+
+
+
+			scanf("%d", &score[j]);
+
+			sum += score[j];
+
+		}
+
+
+
+		avg = (double)sum / (double)N; // ¹Ý Æò±Õ
+
+
+
+		for (k = 0; k < N; k++) {
+
+
+
+			if (avg < score[k])
+
+				cnt++;
+
+		}
+
+
+
+		avg = (double)cnt / (double)N;
+
+
+
+		printf("%.3lf%\n", avg * 100);
+
+	}
+
+}
