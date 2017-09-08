95e447398b828306c3730237b8c1dec747b465fb
Seung Hyeon Jo <chameleon001@naver.com>
2017-09-08 22:11:14 +0900

    20170908 2448번 x축 고치기

--- Project1/1065.cpp
+++ Project1/1065.cpp
@@ -17,9 +17,33 @@ N보다 작거나 같은 한수의 개수를 출력하는 프로그램을 작성하시오.
 
 99
 */
-
 #include<stdio.h>
+int get_hansoo(int n);
 
-int main() {
+int main(void)
+{
+	int input;
+	scanf("%d", &input);
+	printf("%d\n", get_hansoo(input));
+}
 
+int get_hansoo(int n)
+{
+	if (n < 100) return n;
+	else {
+		int i, hc = 99;
+		for (i = 100; i <= n; ++i)
+		{
+			int a[3]; // 1 , 10 , 100
+
+			if (i == 1000) break;
+
+			a[0] = (i % 1000) / 100;
+			a[1] = (i % 100) / 10;
+			a[2] = i % 10;
+
+			if ((a[0] == a[1] && a[0] == a[2]) || a[0] - a[1] == a[1] - a[2]) hc++;
+		}
+		return hc;
+	}
 }
