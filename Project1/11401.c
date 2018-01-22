6076f00cdf36141e3077b8ec64feed199ee781d7
chameleon001 <chameleon001@naver.com>
2018-01-22 21:52:23 +0900

    20180122 구현 및 배열 문제

--- Project1/11401.c
+++ Project1/11401.c
@@ -23,14 +23,23 @@
 */
 
 #include<cstdio>
+#define mod (int)(1e9+7)
 
-int main(){
-    int a,b,c=1,d=1,e;
-    scanf("%d %d",&a,&b);
-    e=b;
-    for(int i =0; i<e; i++){
-        c*=a--;
-       d*=b--; 
-    }
-    printf("%d",(c/d)%1000000007);
+const int MAX_N = 4e6;
+int n, k, fac[MAX_N + 1];
+
+typedef long long ll;
+
+int f(int x, int y) {
+    if (!y) return 1;
+    int t = f(x, y / 2);
+    return (ll)t*t%mod*(y & 1 ? x : 1) % mod;
+}
+
+int main() {
+    scanf("%d %d", &n, &k);
+    fac[0] = 1;
+    for (int i = 1; i <= n; i++) fac[i] = (ll)fac[i - 1] * i%mod;
+    printf("%lld", (ll)fac[n] * f(fac[n - k], mod - 2) % mod*f(fac[k], mod - 2) % mod);
+    return 0;
 }
